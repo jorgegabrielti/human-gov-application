ac6ea6ec4edaff7c2f1be64c04e3b8291244096b
Jorge Gabriel <82737544+jorgegabrielti@users.noreply.github.com>
2025-06-29 17:48:07 -0300

    Add files via upload

--- src/config.py
+++ src/config.py
@@ -1,11 +1,13 @@
 from os import environ as env
 import os
-#import multiprocessing
 
 # App Config
 US_STATE_RAW = os.environ.get('US_STATE')
-US_STATE = ' '.join(word.capitalize() if len(word) > 0 else word for word in US_STATE_RAW.split())
 
+if not US_STATE_RAW:
+    raise RuntimeError("Variável de ambiente US_STATE não definida")
+
+US_STATE = ' '.join(word.capitalize() if len(word) > 0 else word for word in US_STATE_RAW.split())
 
 PORT = int(env.get("PORT", 5000))
 DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
@@ -17,5 +19,5 @@ AWS_DYNAMODB_TABLE = os.environ.get('AWS_DYNAMODB_TABLE')
 
 # Gunicorn config
 bind = ":" + str(PORT)
-#workers = multiprocessing.cpu_count() * 2 + 1
-#threads = 2 * multiprocessing.cpu_count()
+# workers = multiprocessing.cpu_count() * 2 + 1
+# threads = 2 * multiprocessing.cpu_count()
